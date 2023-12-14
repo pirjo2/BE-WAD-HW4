5e12cea153b8b3b0315039efd4ac87f5f4f422d2
pirjo2 <pirjo.joelo@gmail.com>
2023-12-15 00:07:35 +0200

    contact page, but does not work

---
+++ src/views/Contact.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="Contact">
+  <div id="post-list">
+    <p>Contact information</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Contact"
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+a {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+.item {
+  background: rgb(189, 212, 199);
+  margin-bottom: 5px;
+  padding: 3px 5px;
+  border-radius: 10px;
+}
+#post-list {
+  background: #6e8b97;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+  margin: auto;
+  width: 50%;
+  border-radius: 20px;
+}
+#post-list ul {
+  padding: 0;
+}
+#post-list li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
+}
+</style>
